563a52b58495504a903fd498aa2f8b902d6459ad
sanchit sherawat <sanchitsherawat@sanchits-MacBook-Air.local>
2025-04-07 22:53:07 +0530

    api get block quarry ref id fixed

--- src/routes/blockRoutes.ts
+++ src/routes/blockRoutes.ts
@@ -3,9 +3,10 @@ import { createBlock, getBlocks, getBlockById, getBlockByQuarryRefId } from '../
 
 const router = express.Router();
 
+router.get('/quarry/:quarryRefId', getBlockByQuarryRefId);
 router.post('/', createBlock); // POST API to create a block
 router.get('/', getBlocks); // GET API to fetch all blocks
 router.get('/:id', getBlockById); // GET API to fetch a block by ID
-router.get('/block/:quarryRefId', getBlockByQuarryRefId);
+
 
 export default router;
